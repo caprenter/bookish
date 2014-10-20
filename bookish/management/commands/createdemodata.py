084faab745e15f503ba14f98e8536bb72d6166ff
Ben Webb <bjwebb67@googlemail.com>
2014-10-21 00:07:56 +0100

    Use create_user instaed of create in createdemodata
    This ensures that the password is hashed correctly.

--- bookish/management/commands/createdemodata.py
+++ bookish/management/commands/createdemodata.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User.objects.create_superuser(username='demo_admin', password='demo_admin', email='demo@example.org')
-        demo_accountant = User.objects.create(username='demo_accountant', password='demo_accountant')
-        demo_client = User.objects.create(username='demo_client', password='demo_client')
+        demo_accountant = User.objects.create_user(username='demo_accountant', password='demo_accountant')
+        demo_client = User.objects.create_user(username='demo_client', password='demo_client')
 
         accountancy_firm = m.AccountancyFirm.objects.create()
         accountancy_firm.users.add(demo_accountant)
