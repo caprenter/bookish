c579d181df118e3c7697772de874d49deb92a3d7
Ben Webb <bjwebb67@googlemail.com>
2014-12-19 12:48:09 +0000

    Add a page that errors to test sentry

--- bookish/urls.py
+++ bookish/urls.py
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^cash/revision/(?P<transaction_id>[^/]+)/$', login_required(v.CashItemRevisionView.as_view()), name='bookish-cash_revison_list'),
     url(r'^accountancy-firm$', login_required(v.AccountancyFirmListView.as_view()), name='bookish-accountancy-firm_list'),
     url(r'^company$', login_required(v.CompanyListView.as_view()), name='bookish-company_list'),
+    url(r'^test500$', lambda: None),
 )
